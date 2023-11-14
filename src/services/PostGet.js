8264cae62ef41cf451d14d0810d1de9b470aab79
hunk12369 <kevinbryanmora2@hotmail.com>
2023-11-14 03:28:50 -0400

    feat: login autentification

--- src/services/PostGet.js
+++ src/services/PostGet.js
@@ -20,12 +20,7 @@ export const postUsuario = async (usuarioNuevo = "") => {
 };
 
 
-export const loginOk = async () => {
-    const credentials = {
-      username: username,
-      password: password,
-    };
-
+export const loginOk = async (credentials) => {
     try {
       const response = await fetch("http://localhost/matrix_bd/usuarios.php", {
         method: 'POST',
@@ -38,15 +33,23 @@ export const loginOk = async () => {
       if (response.ok) {
         // Autenticación exitosa
         const data = await response.json();
-        
-        //console.log('Token de autenticación:', data.token);
+        console.log(data);
+        if (data.response == 'true'){
+          return true;
+        } else {
+          return false;
+        }
+//        console.log('Token de autenticación:', 'kj2');
         // Puedes almacenar el token en el estado o en una cookie, según tus necesidades
       } else {
         // Autenticación fallida
         const errorData = await response.json();
         console.error('Error de autenticación:', errorData.message);
+        return false;
       }
     } catch (error) {
       console.error('Error de red:', error.message);
+      return false;
+
     }
   };
